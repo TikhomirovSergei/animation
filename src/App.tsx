d04519a34e0bec135852e614f48d2ea7a39d88d3
Sergey Tihomirov <truebelievervalar@gmail.com>
2023-09-16 15:17:34 +0300

    add libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,27 @@
-import React, { type PropsWithChildren } from 'react';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {
-  SafeAreaView,
   ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  View
 } from 'react-native';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({ children, title }) => {
-  // const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={[styles.sectionTitle]}>{title}</Text>
-      <Text style={[styles.sectionDescription]}>{children}</Text>
-    </View>
-  );
-};
+import { Header } from '@rneui/themed';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    <SafeAreaProvider>
+      <Header
+        leftComponent={{
+          icon: 'menu',
+          color: '#fff'
+        }}
+        centerComponent={{ text: 'Header' }}
+      />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
         </View>
       </ScrollView>
-    </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-});
 
 export default App;
